f5467d5cad08da138bc603ed5c6d24ff0f96c727
Hou <vivhou@gmail.com>
2017-05-25 16:27:09 -0400

    fixed path issue in app.js file

--- template_files/app.js
+++ template_files/app.js
@@ -1,5 +1,6 @@
 console.log('hi')
-console.log(window.location.pathname.slice(1, -5))
+var page = window.location.pathname.split("/").pop(-1)
+var category = page.slice(0,-5)
 
 var app = angular.module('navList', []);
 
@@ -12,7 +13,7 @@ var app = angular.module('navList', []);
 
 app.controller('navCtrl', ['$scope', function ($scope) {
     $scope.navClass = function (page) {
-        var currentRoute = window.location.pathname.slice(1, -5) || 'intro';
+        var currentRoute = category || 'intro';
         return page === currentRoute ? 'hovered-category' : '';
     };        
 }]);
