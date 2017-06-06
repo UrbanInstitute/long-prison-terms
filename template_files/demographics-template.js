9613cab56f5eb2129aa28413f6694a61b3655666
Ben Chartoff <bchartoff@gmail.com>
2017-06-06 16:34:41 -0500

    fixing scroll animation transitions

--- template_files/demographics-template.js
+++ template_files/demographics-template.js
@@ -1,4 +1,3 @@
-console.log(window.scrollY)
 
 
 var duration = 300; 
@@ -15,31 +14,25 @@ var arrowTransitionMin = 200,
     transition8Max = 6300,
     transition9Max = 6700;
 
-var isTransitioning_1 = false,
-    isTransitioning_2 = false,
-    isTransitioning_3 = false,
-    isTransitioning_4 = false,
-    isTransitioning_5 = false,
-    isTransitioning_6 = false,
-    isTransitioning_7 = false,
-    isTransitioning_8 = false,
-    isTransitioning_9 = false;
+var isTransitioning = [null, false, false, false, false, false, false, false, false, false]
 
+function quoteTransition(number) { 
 
-function quoteTransition(number, transitionNumber) { 
-
-    // var transitionNumber0 = "isTransitioning_" + number
+    // var transitionNumber0 = "isTransitioning["] + number
     // var transitionNumber = eval(transitionNumber0)
     d3.select(".dem-quote" + number)
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ transitionNumber = true; console.log(transitionNumber)})
-                .on("end", function(){ transitionNumber = false })
+        .transition()
+        .duration(duration)
+        .style("opacity", 1)
+        .on("start", function(){ console.log(number); isTransitioning[number] = true })
+        .on("end", function(){ console.log(number); isTransitioning = [null, false, false, false, false, false, false, false, false, false] })
+    d3.selectAll(".note-container:not(.dem-quote" + number + ")")
+        .transition()
+        .duration(duration)
+        .style("opacity", 0)
 }
 
 window.onscroll = function() {
-    console.log(window.scrollY)
     if(window.scrollY < arrowTransitionMin){
         d3.select("#introArrow")
             .style("opacity", 1)
@@ -51,157 +44,73 @@ window.onscroll = function() {
             .style("opacity", 0)
         d3.select("#introArrow")
             .style("opacity", 0)
-    } else if(window.scrollY >= arrowTransitionMax && window.scrollY <transition1Max){     console.log(isTransitioning_1) 
+    } else if(window.scrollY >= arrowTransitionMax && window.scrollY <transition1Max){     
         //FIRST QUOTE APPEARS
-        if (!isTransitioning_1)  {        
-            d3.select(".dem-quote2")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-        quoteTransition(1, isTransitioning_1)
-
+        if (!isTransitioning[1])  {
+            quoteTransition(1)
         }  
  
         
 
     } else if(window.scrollY >= transition1Max && window.scrollY <transition2Max){ 
         //SECOND QUOTE APPEARS
-        if (!isTransitioning_2) {  console.log(isTransitioning_2)
-            d3.selectAll(".dem-quote1, .dem-quote3")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            quoteTransition(2, isTransitioning_2)
+        if (!isTransitioning[2]) {  
+            quoteTransition(2)
         } 
-        // else if (d3.select('.dem-quote2').classed('done') == true) {
-        //      d3.selectAll(".dem-quote1, .dem-quote3")
-        //         .transition()
-        //         .duration(duration)
-        //         .style("opacity", 0)
-        //    quoteTransition(2)
-        // }
-        
+
  
-             console.log(isTransitioning_2)
         
 
     } else if(window.scrollY >= transition2Max && window.scrollY <transition3Max){  
         //THIRD QUOTE APPEARS
-        if (!isTransitioning_3) {
-            d3.selectAll(".dem-quote2, .dem-quote4")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote3')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_3 = true})
-                .on("end", function(){ isTransitioning_3 = false})
-            } 
-            isTransitioning_3 = false
+        if (!isTransitioning[3]) {
+            quoteTransition(3)
+        } 
+            
         
 
     } else if(window.scrollY >= transition3Max && window.scrollY <transition4Max){ 
     	        //FOURTH QUOTE APPEARS
-        if (!isTransitioning_4) {
-           d3.selectAll(".dem-quote3, .dem-quote5")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote4')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_4 = true})
-                .on("end", function(){ isTransitioning_4 = false})
-            } 
-            isTransitioning_4 = false
+        if (!isTransitioning[4]) {
+            quoteTransition(4)
+        } 
         
 
     } else if(window.scrollY >= transition4Max && window.scrollY <transition5Max){ 
     	        //FIFTH QUOTE APPEARS
-        if (!isTransitioning_5) {
-            d3.selectAll(".dem-quote4, .dem-quote6")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote5')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_5 = true})
-                .on("end", function(){ isTransitioning_5 = false})
-            } 
-            isTransitioning_5 = false
+        if (!isTransitioning[5]) {
+            quoteTransition(5)
+        } 
         
 
     } else if(window.scrollY >= transition5Max && window.scrollY <transition6Max){ 
                 //SIXTH QUOTE APPEARS
-        if (!isTransitioning_6) {
-            d3.selectAll(".dem-quote5, .dem-quote7")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote6')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_6 = true})
-                .on("end", function(){ isTransitioning_6 = false})
-            } 
-            isTransitioning_6 = false
+        if (!isTransitioning[6]) {
+            quoteTransition(6)
+        } 
         
 
     } else if(window.scrollY >= transition6Max && window.scrollY <transition7Max){ 
                 //SEVENTH QUOTE APPEARS
-        if (!isTransitioning_7) {
-            d3.selectAll(".dem-quote6, .dem-quote8")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote7')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_7 = true})
-                .on("end", function(){ isTransitioning_7 = false})
-            } 
-            isTransitioning_7 = false
+        if (!isTransitioning[7]) {
+            quoteTransition(7)
+        } 
         
 
     } else if(window.scrollY >= transition7Max && window.scrollY <transition8Max){ 
                 //EIGHTH QUOTE APPEARS
-        if (!isTransitioning_8) {
-            d3.selectAll(".dem-quote7, .dem-quote9")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote8')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_8 = true})
-                .on("end", function(){ isTransitioning_8 = false})
-            } 
-            isTransitioning_8 = false
+        if (!isTransitioning[8]) {
+            console.log("foo")
+            quoteTransition(8)
+        } 
+
         
 
     } else if(window.scrollY >= transition8Max && window.scrollY < transition9Max){ 
                 //NINTH QUOTE APPEARS
-        if (!isTransitioning_9) {
-            d3.selectAll(".dem-quote8")
-                .transition()
-                .duration(duration)
-                .style("opacity", 0)
-            d3.select('.dem-quote9')
-                .transition()
-                .duration(duration)
-                    .style("opacity", 1)
-                .on("start", function(){ isTransitioning_9 = true})
-                .on("end", function(){ isTransitioning_9 = false})
+        if (!isTransitioning[9]) {
+            quoteTransition(9)
         } 
-            isTransitioning_9 = false
         
 
     }     
