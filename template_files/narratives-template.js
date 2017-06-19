ca00fa363ac85a4f59ca6940287999b284f0d565
Hou <vivhou@gmail.com>
2017-06-19 09:37:14 -0400

    added edits to next section nav text, and serena's edits to consequences and reform pages

--- template_files/narratives-template.js
+++ template_files/narratives-template.js
@@ -52,16 +52,22 @@ window.onscroll = function() {
             quoteTransition(1)
         }  
     }
-    else if (getViewMin("#sidebar3") <= inViewMin && getViewMin("#sidebar4") > inViewMin) {
+    else if (getViewMin("#sidebar3") <= inViewMin && getViewMin("#sidebar4") > inViewMin) { 
         //SIDEBAR 3 APPEARS
         if (!isTransitioning[3]) {
             quoteTransition(3)
         } 
     }
-    else if (getViewMin("#sidebar4") <= inViewMin && getViewMin("#sidebar6") > inViewMin) {
+    else if (getViewMin("#sidebar4") <= inViewMin && getViewMin("#sidebar2") > inViewMin) { console.log('4')
         //SIDEBAR 4 APPEARS
         if (!isTransitioning[4]) {
             quoteTransition(4)
+        } 
+    }
+    else if (getViewMin("#sidebar2") <= inViewMin && getViewMin("#sidebar6") > inViewMin) { console.log('2')
+        //SIDEBAR 2 APPEARS
+        if (!isTransitioning[2]) {
+            quoteTransition(2)
         } 
     }
     else if (getViewMin("#sidebar6") <= inViewMin && getViewMin("#sidebar7") > inViewMin) {
