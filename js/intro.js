80f1b4c3467ba532448becda48f951cda8354270
Ben Chartoff <bchartoff@gmail.com>
2017-07-05 13:39:52 -0500

    swipe intro to switch animation to line chart

--- js/intro.js
+++ js/intro.js
@@ -2369,7 +2369,6 @@ var yDown = null;
 
 function handleTouchStart(evt) {                                         
     xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
 };                                                
 
 function handleTouchMove(evt) {
@@ -2378,10 +2377,8 @@ function handleTouchMove(evt) {
     }
 
     var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
 
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
@@ -2395,14 +2392,7 @@ function handleTouchMove(evt) {
               showLine();
             }
         }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
-            /* down swipe */
-        }                                                                 
     }
-    /* reset values */
     xDown = null;
     yDown = null;                                             
 };
