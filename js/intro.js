06fb35d68bf8a593d84781609ba2faa71199c056
Ben Chartoff <bchartoff@gmail.com>
2017-07-05 13:30:34 -0500

    swipe handler for intro

--- js/intro.js
+++ js/intro.js
@@ -208,22 +208,22 @@ var scrollVis = function() {
   var AREA_ANIMATING = false;
   var WIDTH, LINEWIDTH, HEIGHT, LINEHEIGHT;
   if(IS_PHONE()){
-    WIDTH = window.innerWidth - 100 + 35;
+    WIDTH = window.innerWidth - 100 + 35 + 25;
     LINEWIDTH = window.innerWidth - 63 + 14; 
   }
   else if(IS_TABLET()){
-    WIDTH = window.innerWidth - 240 + 35;
+    WIDTH = window.innerWidth - 240 + 35 + 25;
     LINEWIDTH = window.innerWidth - 240 - 14; 
   }
   else if(IS_MOBILE()){
-    WIDTH = 370;
+    WIDTH = 370 +25;
     LINEWIDTH = 390;
   }
   else if (BREAK1()){
-    WIDTH = 370;
+    WIDTH = 370 +25;
     LINEWIDTH = 390;
   }else{
-    WIDTH = 420;
+    WIDTH = 420 +25;
     LINEWIDTH = 390;
   }
 
@@ -234,7 +234,7 @@ var scrollVis = function() {
     HEIGHT = 517;
     LINEHEIGHT = 537
   }
-  var margin = {top: 72, right: 5, bottom: 10, left: 25},
+  var margin = {top: 72, right: 30, bottom: 10, left: 25},
     width = WIDTH - margin.left - margin.right,
     height = HEIGHT - margin.top - margin.bottom
 
@@ -1014,7 +1014,8 @@ var scrollVis = function() {
         .attr("transform",function(){
           var xx = node.getBoundingClientRect().left
           var xy = node.getBoundingClientRect().top
-          var angle = (Math.round(Math.random()) * 2 - 1) * Math.random() * 100
+          var angle = Math.abs((Math.round(Math.random()) * 2 - 1) * Math.random() * 100)
+          // console.log(angle)
           var dist = Math.random() * 1000
           return "translate(" +dist+ ") rotate(" + angle + " " + xx + " " + xy + ")"
         })
@@ -2356,3 +2357,52 @@ nextpage
         nextpage.select(".next-arrow-hovered")
             .attr("class", "next-arrow")
     })
+
+
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+            if(parseInt(d3.select("#mobileSwitchContainer").style("opacity")) == 1){
+              showAnimation();
+            }
+        } else {
+            /* right swipe */
+            if(parseInt(d3.select("#mobileSwitchContainer").style("opacity")) == 1){
+              showLine();
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
